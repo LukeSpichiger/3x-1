fcbba0afd4307635ab610489c5123f0346c8d299
LukeSpichiger <155106985+LukeSpichiger@users.noreply.github.com>
2024-01-02 07:43:13 +0100

    First working version

--- main.c
+++ main.c
@@ -1,37 +1,59 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define MAX_NUMBERS 100
-#define MAX_ITERATIONS 1000
-#define HIGHEST_NUMBER 100
+#define MAX_ITERATIONS 1000000
+#define HIGHEST_NUMBER 10
 
 int Collatz(int number);
 int GetRandomNumber(int min, int max);
 
-char program_entry[] = {"3x+1 PROGRAMM!\n"};
-
 int main(int argc, char *argv[]) {
-  int max_iterations = MAX_ITERATIONS;
-  int number_array[MAX_NUMBERS];
+  int number_array[HIGHEST_NUMBER];
+  bool dataset_grade = true;
   printf("3x+1 PROGRAMM!\n\n");
-  printf("Generating Random Numbers:\n");
-  for (size_t i = 0; i < MAX_NUMBERS; i++) {
-    number_array[i] = GetRandomNumber(0, HIGHEST_NUMBER);
+  printf("Generating Numbers:\n");
+  for (size_t i = 0; i < HIGHEST_NUMBER; i++) {
+    number_array[i] = i + 1;
   }
   printf("Done!\n\n");
   printf("Calculating Data:\n");
+
+  for (size_t i = 0; i < HIGHEST_NUMBER; i++) {
+    number_array[i] = Collatz(number_array[i]);
+  }
+  printf("Done!\n\n");
+  printf("Printing Data:\n");
+  for (size_t i = 0; i < HIGHEST_NUMBER; i++) {
+    printf("%d\n", number_array[i]);
+  }
+  printf("Done!\n\n");
+  printf("Judging Data:\n");
+  for (size_t i = 0; i < HIGHEST_NUMBER; i++) {
+    if (number_array[i] != 1) {
+      printf("ANOMALY FOUND!\n");
+      dataset_grade = 0;
+    }
+  }
+  if (dataset_grade == true) {
+    printf("Dataset OK!\n");
+  } else {
+    printf("Dataset ANOMALY\n");
+  }
+  return 0;
 }
 
 int Collatz(int number) {
-  int modulo;
-  int result;
-  modulo = number % 2;
-
-  if (modulo) {
-    result = number / 2;
-  } else {
-    result = (3 * number) + 1;
+  int result = number;
+  int iterator = 0;
+  while (result != 1 && iterator < MAX_ITERATIONS) {
+    if (result % 2 == 0) {
+      result /= 2;
+    } else {
+      result = (result * 3) + 1;
+    }
+    iterator++;
   }
   return result;
 }
